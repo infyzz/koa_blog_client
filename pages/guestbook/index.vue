52d7a834f50649e5c5fe9d556f4c18a67700e99b
mr.zhao <712733712@qq.com>
2020-11-06 23:08:18 +0800

    主体搭建

---
+++ pages/guestbook/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="main-guestbook wrapper">
+        <main class="main-guestbook">
+            <!-- 评论 -->
+            <client-only>
+                <app-comment />
+            </client-only>
+        </main>
+    </div>
+</template>
+<script>
+import Vue from 'vue';
+// import { value, computed, watch, onMounted } from 'vue-function-api';
+export default {
+    components: {
+        'app-comment': () => import(/*webpackChunkName: "app-comment"*/'~/components/public/AppComment')
+    },
+    
+};
+</script>
